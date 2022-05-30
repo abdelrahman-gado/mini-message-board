af3806a75087d0872a8f177fc544ca2082f284fa
abdelrahman <gadoua50@gmail.com>
2022-05-30 23:54:35 +0200

    finish /new page

--- routes/index.js
+++ routes/index.js
@@ -20,4 +20,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: "Mini Messageboard", messages });
 });
 
+router.get('/new', (req, res) => {
+  res.render('form');
+})
+
 module.exports = router;
